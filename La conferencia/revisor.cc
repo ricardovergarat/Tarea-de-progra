c3b2401ea6123f4b7193a8f07c76f87a30bae82d
ricardovergarat <ricardovergarat1@gmail.com>
2020-01-19 01:45:29 -0300

    ahora se guardo el proyecto

--- La conferencia/revisor.cc
+++
@@ -1,8 +0,0 @@
-# include "revisor.h"
-# include "persona.h"
-
-using namespace std;
-
-revisor::revisor(string _nombre, string _afiliacion, string _correo):persona(_nombre,_afiliacion,_correo){
-
-}
