0c41aee2cbf58b1abcb230bd2125e79b5d332709
ricardovergarat <ricardovergarat1@gmail.com>
2020-02-09 14:15:28 -0300

    se contruyeron los articulos

--- NP/main.cc
+++ NP/main.cc
@@ -93,83 +93,84 @@ void agregar_conferencias_falsas(vector <conferencia> & lista){
 	
 	vector <string> comentarios;
 	vector <int> notas;
-	comentarios = comentarios_falsos("djasiodjasldjlasidj","dkfjodifu wirueirou weirouw eri9pu","dfjfh");
+	comentarios = comentarios_falsos("me gustan los piratas","yo vi los piratas del caribe","no entendi un carajo pero el autor me pago");
 	notas = notas_falsas(3,5,3);
 	
 	articulo ar("piratas","texto de piratas","los piratas eran ladrones hace muchos años",comentarios,notas);
 	
-	comentarios = comentarios_falsos("djasiodjasldjlasidj","dkfjodifu wirueirou weirouw eri9pu","sds aldhasduh asildh laskdh askljdh jfh");
+	comentarios = comentarios_falsos("solo me gusta dragon ball","origen no muy bien explicado","sus datos estan bien explicados");
 	notas = notas_falsas(2,5,3);
 	articulo ar1("anime","series animadas","forma de entretenimiento creada en japon",comentarios,notas);
 	
-	comentarios = comentarios_falsos("djasiodjasldjlasidj","dkfjodifu wirueirou weirouw eri9pu","sds aldhasduh asildh laskdh askljdh jfh");
+	comentarios = comentarios_falsos("ahora entiendo las peliculas","alfin entendi como sobrevir","con razon todos siempre mueren");
 	notas = notas_falsas(4,2,1);
-	articulo ar2("zombies","series animadas","forma de entretenimiento creada en japon",comentarios,notas);
+	articulo ar2("zombies","habla los tipos de zombies","un tipo de mostruo dificil de matar",comentarios,notas);
 	
-	comentarios = comentarios_falsos("djasiodjasldjlasidj","dkfjodifu wirueirou weirouw eri9pu","sds aldhasduh asildh laskdh askljdh jfh");
+	comentarios = comentarios_falsos("no puedo parglo","es muy bueno","falta ortografia");
 	notas = notas_falsas(2,5,2);
-	articulo ar2("netflix","series animadas","forma de entretenimiento creada en japon",comentarios,notas);
+	articulo ar3("netflix","aplicacion para videos","esta aplicaion tiene muchas pelicuas y series",comentarios,notas);
 	
-	comentarios = comentarios_falsos("djasiodjasldjlasidj","dkfjodifu wirueirou weirouw eri9pu","sds aldhasduh asildh laskdh askljdh jfh");
+	comentarios = comentarios_falsos("es malo para los jovenes","tiene muchos insultos","los autores no saben escribir");
 	notas = notas_falsas(3,1,1);
-	articulo ar3("league of legends","series animadas","forma de entretenimiento creada en japon",comentarios,notas);
+	articulo ar4("league of legends","es un juego","es juego creado en el 2010",comentarios,notas);
 	
-	comentarios = comentarios_falsos("djasiodjasldjlasidj","dkfjodifu wirueirou weirouw eri9pu","sds aldhasduh asildh laskdh askljdh jfh");
+	comentarios = comentarios_falsos("no esta bien explicado","no explica origen","faltan imagenes");
 	notas = notas_falsas(1,5,3);
-	articulo ar4("idiomas","series animadas","forma de entretenimiento creada en japon",comentarios,notas);
+	articulo ar5("idiomas","la forma de comunicarse","existe una infinidad de idiomas y se pueden crear",comentarios,notas);
 	
-	comentarios = comentarios_falsos("djasiodjasldjlasidj","dkfjodifu wirueirou weirouw eri9pu","sds aldhasduh asildh laskdh askljdh jfh");
+	comentarios = comentarios_falsos("no queda claro el origen","faltan referencias","no explica como se crean");
 	notas = notas_falsas(2,3,3);
-	articulo ar5("peliculas","series animadas","forma de entretenimiento creada en japon",comentarios,notas);
+	articulo ar6("peliculas","videos de larga duracion","video que cuentan historia por medio de imagenes",comentarios,notas);
 	
-	comentarios = comentarios_falsos("djasiodjasldjlasidj","dkfjodifu wirueirou weirouw eri9pu","sds aldhasduh asildh laskdh askljdh jfh");
+	comentarios = comentarios_falsos("no explica que es","no menciona referencias","faltan ejemplos");
 	notas = notas_falsas(1,4,5);
-	articulo ar6("series","series animadas","forma de entretenimiento creada en japon",comentarios,notas);
+	articulo ar7("series","video de duracion media","cuentas una historia atravez de varios capitulos",comentarios,notas);
 	
-	comentarios = comentarios_falsos("djasiodjasldjlasidj","dkfjodifu wirueirou weirouw eri9pu","sds aldhasduh asildh laskdh askljdh jfh");
+	comentarios = comentarios_falsos("no quedan claras las epocas","no se extiende el capitulo 5","faltan imagenes");
 	notas = notas_falsas(2,4,5);
-	articulo ar7("the big bang theory","series animadas","forma de entretenimiento creada en japon",comentarios,notas);
+	articulo ar8("the big bang theory","teoria del origen del universo","teoria que propone una explosion a bilones de años",comentarios,notas);
 	
-	comentarios = comentarios_falsos("djasiodjasldjlasidj","dkfjodifu wirueirou weirouw eri9pu","sds aldhasduh asildh laskdh askljdh jfh");
+	comentarios = comentarios_falsos("no menciona a los espartanos","odio este articulo","por estos articulos canlan la psu");
 	notas = notas_falsas(1,4,4);
-	articulo ar8("","series animadas","forma de entretenimiento creada en japon",comentarios,notas);
+	articulo ar9("historia","tiene la historia del mundo","la historia comezo a ser reistrada en el año ...",comentarios,notas);
 	
-	comentarios = comentarios_falsos("djasiodjasldjlasidj","dkfjodifu wirueirou weirouw eri9pu","sds aldhasduh asildh laskdh askljdh jfh");
+	comentarios = comentarios_falsos("explica bien los numeros","es muy basico","no explica los cardinales");
 	notas = notas_falsas(1,1,1);
-	articulo ar9("anime","series animadas","forma de entretenimiento creada en japon",comentarios,notas);
+	articulo ar10("matematicas","introduce a la matematica","es el uso de numeros para resolver problemas",comentarios,notas);
 	
-	comentarios = comentarios_falsos("djasiodjasldjlasidj","dkfjodifu wirueirou weirouw eri9pu","sds aldhasduh asildh laskdh askljdh jfh");
+	comentarios = comentarios_falsos("no meciona los idomas","no explica origen idomas","no explica importancia");
 	notas = notas_falsas(4,5,3);
-	articulo ar10("anime","series animadas","forma de entretenimiento creada en japon",comentarios,notas);
+	articulo ar11("lenguas muertas","historia idomas que ya no se hablan","esto se debe a que quien conocen el idoma no pudieron eneñarlo",comentarios,notas);
 	
-	comentarios = comentarios_falsos("djasiodjasldjlasidj","dkfjodifu wirueirou weirouw eri9pu","sds aldhasduh asildh laskdh askljdh jfh");
+	comentarios = comentarios_falsos("faltan dibijos","abusa de los ejemplos","no muy avanzado");
 	notas = notas_falsas(3,5,1);
-	articulo ar11("anime","series animadas","forma de entretenimiento creada en japon",comentarios,notas);
+	articulo ar12("codigo binario","explica funcionamiento de los bits","los aparatos funcionan con ausencia o presencia de electricidad",comentarios,notas);
 	
-	comentarios = comentarios_falsos("djasiodjasldjlasidj","dkfjodifu wirueirou weirouw eri9pu","sds aldhasduh asildh laskdh askljdh jfh");
+	comentarios = comentarios_falsos("falta mas teoria","solo explica autos mecanicos","no explica importancia");
 	notas = notas_falsas(1,5,4);
-	articulo ar12("anime","series animadas","forma de entretenimiento creada en japon",comentarios,notas);
+	articulo ar13("cueso de conducion","explica reglas para conducir","este sistema fue creada para previnir las siguies situaciones",comentarios,notas);
 	
-	comentarios = comentarios_falsos("djasiodjasldjlasidj","dkfjodifu wirueirou weirouw eri9pu","sds aldhasduh asildh laskdh askljdh jfh");
+	comentarios = comentarios_falsos("sub grupos no claros","faltan estadisticas","explica bien los porque");
 	notas = notas_falsas(2,5,4);
-	articulo ar13("anime","series animadas","forma de entretenimiento creada en japon",comentarios,notas);
+	articulo ar14("tipos de aprendizaje","explica forma de apreneder","la capacidad de aprender se divide en grupo",comentarios,notas);
 	
-	comentarios = comentarios_falsos("djasiodjasldjlasidj","dkfjodifu wirueirou weirouw eri9pu","sds aldhasduh asildh laskdh askljdh jfh");
+	comentarios = comentarios_falsos("hace buen uso de los ejemplos","falta tecnicas","es muy corto el articulo");
 	notas = notas_falsas(2,5,2);
-	articulo ar14("anime","series animadas","forma de entretenimiento creada en japon",comentarios,notas);
+	articulo ar15("expresionn oral","explica tecnicas de comunicacion","la comunicacion tiene la finalidad de expresar",comentarios,notas);
 	
-	comentarios = comentarios_falsos("djasiodjasldjlasidj","dkfjodifu wirueirou weirouw eri9pu","sds aldhasduh asildh laskdh askljdh jfh");
+	comentarios = comentarios_falsos("no hay ejemplos","es muy avanzado para principiantes","falta referencias");
 	notas = notas_falsas(1,3,5);
-	articulo ar1(5"anime","series animadas","forma de entretenimiento creada en japon",comentarios,notas);
+	articulo ar16("paginas web","mestra tecnicas de desarrolo web","el desarrolo se basa en la ceracion de objetos en lenguajes",comentarios,notas);
 	
-	comentarios = comentarios_falsos("djasiodjasldjlasidj","dkfjodifu wirueirou weirouw eri9pu","sds aldhasduh asildh laskdh askljdh jfh");
+	comentarios = comentarios_falsos("no entendi nada","por esto estudie lenguaje","apenas se entendio");
 	notas = notas_falsas(2,3,4);
-	articulo ar16("anime","series animadas","forma de entretenimiento creada en japon",comentarios,notas);
+	articulo ar17("matrices","introduce al algebra lineal","son un conjunto de sistema de incognitas",comentarios,notas);
 	
-	comentarios = comentarios_falsos("djasiodjasldjlasidj","dkfjodifu wirueirou weirouw eri9pu","sds aldhasduh asildh laskdh askljdh jfh");
+	comentarios = comentarios_falsos("tiene buenas imagens","es muy largo","no estan bien divididos los capitulos");
 	notas = notas_falsas(3,4,3);
-	articulo ar17("anime","series animadas","forma de entretenimiento creada en japon",comentarios,notas);
+	articulo ar18("calculo diferencial","introdice al calculo","explicaremos primero el teorema fundamentas del calulo",comentarios,notas);
 	
+	ar.mostrar_articulo();
 	
 	
 	
